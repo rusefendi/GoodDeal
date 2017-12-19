5fe9dfc7c5109a07703a844ddb481abed628c01b
Efendiev Ruslan <rusefendi@gmail.com>
2017-12-19 23:41:12 +0300

    updated chat_id usage

--- telegram.py
+++ telegram.py
@@ -3,30 +3,16 @@ import requests
 
 # your Telegram bot token
 token = misc.token
+chat_id = misc.chat_id
 URL = 'https://api.telegram.org/bot' + token + '/'
 
 
-def send_message(chat_id, text):
+def send_message(text):
     url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
     requests.get(url)
-
-
-def get_updates():
-    url = URL + 'getupdates'
-    r = requests.get(url)
-    return r.json()
-
-
-# requested for sending messages
-def get_chat_id():
-    data = get_updates()
-
-    chat_id = data['result'][-1]['message']['chat']['id']
-
-    return chat_id
 
 
 def post(data):
     text = data['title'] + '\n' + data['price'] + '\n' + data['metro'] + '\n' + data['url']
 
-    send_message(get_chat_id(), text)
+    send_message(text)
